11f23d962dff59e00c41e2e9024915d6f98bbcb0
Nail Nabiev <nabiev-nail@mail.ru>
2025-11-04 11:34:49 +0300

    fixed the flaws

--- index.php
+++ index.php
@@ -7,27 +7,27 @@ session_start();
 $url = $_SERVER['REQUEST_URI'];
 
 if (preg_match('#^/admin$#', $url)) {
-    include './admin.php';
+    include './controllers/admin.php';
     include "./views/admin.php";
 }
 if (preg_match('#^/admin-menu$#', $url)) {
-    include './admin-menu.php';
+    include './controllers/admin-menu.php';
     include "./views/admin-menu.php";
 }
 
 if (preg_match('#^/(edit)/(\d+)$#', $url, $match)) {
     $id = $match[2];
-    include 'edit-film.php';
+    include './controllers/edit-film.php';
     include "./views/edit-film.php";
 }
 
 if (preg_match('#^/$#', $url)) {
-    include 'films.php';
+    include './controllers/films.php';
     include "./views/films.php";
 }
 
 if (preg_match('#^/logout$#', $url)) {
-    include "./logout.php";
+    include "./controllers/logout.php";
 }
 
 
